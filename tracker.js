7e543ffdb065d6fe7646dad8667f7987123f5dc0
JayArghArgh <jayarghargh@gmail.com>
2021-02-11 17:13:46 +1000

    adding menu options to main menu

--- tracker.js
+++ tracker.js
@@ -1,11 +1,14 @@
 "use strict";
 const inquirer = require('inquirer');
 const mysql = require('mysql');
-const ADD = "ADD";
+const ADD_DEPT = "Add Department";
+const ADD_ROLE = "Add Role";
+const ADD_EMP = "Add Employee";
 const VIEW = "VIEW";
 const UPDATE = "UPDATE";
 
 const connection = mysql.createConnection({
+	// Connect to Local database
 	host: 'localhost',
 	port: 3306,
 	user: 'root',
@@ -14,6 +17,7 @@ const connection = mysql.createConnection({
 });
 
 const addItem = (tableName, itemsToAdd) => {
+	// Take an object and add it to the specified table.
 	const query = "INSERT INTO ?? SET ?";
 	let values = itemsToAdd;
 	connection.query(query, [tableName, values], (err, result) => {
@@ -37,17 +41,25 @@ const updateItem = (tableName) => {
 };
 
 const mainMenu = () => {
+	// Display the main menu for the user.
 	inquirer.prompt(
 		{
 			name: 'selectAction',
 			type: 'list',
 			message: 'Please select an action',
-			choices: [ADD, VIEW, UPDATE],
+			choices: [ADD_DEPT, ADD_ROLE, ADD_EMP, new inquirer.Separator(), VIEW, UPDATE],
 		}
 	).then((answer) => {
+		// perform selected action.
 		switch (answer.selectAction) {
-			case (ADD):
-				console.log("adding");
+			case (ADD_DEPT):
+				console.log(ADD_DEPT);
+				break;
+			case (ADD_ROLE):
+				console.log(ADD_ROLE);
+				break;
+			case (ADD_EMP):
+				console.log(ADD_EMP);
 				break;
 			case (VIEW):
 				console.log("viewin");
@@ -63,12 +75,15 @@ const mainMenu = () => {
 	});
 }
 
-connection.connect((err) => {
-	if(err) throw err;
-	// addItem("department", {title: "survey"});
-	// addItem("role", {title: "Surveyor", salary: 99999.99, department_id: 1});
-	// addItem("employee",
-	// 	{first_name: "Frank", last_name: "Beans", role_id: 1, manager_id: 1});
-	// connection.end();
-	mainMenu();
-});
+// connection.connect((err) => {
+// 	if(err) throw err;
+// 	// addItem("department", {title: "survey"});
+// 	// addItem("role", {title: "Surveyor", salary: 99999.99, department_id: 1});
+// 	// addItem("employee",
+// 	// 	{first_name: "Frank", last_name: "Beans", role_id: 1, manager_id: 1});
+// 	// connection.end();
+// 	mainMenu();
+// });
+
+
+mainMenu();
