fa224b3b483132323fd09388059aad280a518979
jayarghargh <jayarghargh@gmail.com>
2021-02-11 16:38:20 +1000

    added basic program branching

--- tracker.js
+++ tracker.js
@@ -1,6 +1,9 @@
 "use strict";
 const inquirer = require('inquirer');
 const mysql = require('mysql');
+const ADD = "ADD";
+const VIEW = "VIEW";
+const UPDATE = "UPDATE";
 
 const connection = mysql.createConnection({
 	host: 'localhost',
@@ -33,11 +36,39 @@ const updateItem = (tableName) => {
 	})
 };
 
+const mainMenu = () => {
+	inquirer.prompt(
+		{
+			name: 'selectAction',
+			type: 'list',
+			message: 'Please select an action',
+			choices: [ADD, VIEW, UPDATE],
+		}
+	).then((answer) => {
+		switch (answer.selectAction) {
+			case (ADD):
+				console.log("adding");
+				break;
+			case (VIEW):
+				console.log("viewin");
+				break;
+			case (UPDATE):
+				console.log("updating");
+				break;
+			default:
+				console.log("peace!");
+				break;
+		}
+		connection.end();
+	});
+}
+
 connection.connect((err) => {
 	if(err) throw err;
 	// addItem("department", {title: "survey"});
 	// addItem("role", {title: "Surveyor", salary: 99999.99, department_id: 1});
-	addItem("employee",
-		{first_name: "Frank", last_name: "Beans", role_id: 1, manager_id: 1});
-	connection.end();
+	// addItem("employee",
+	// 	{first_name: "Frank", last_name: "Beans", role_id: 1, manager_id: 1});
+	// connection.end();
+	mainMenu();
 });
